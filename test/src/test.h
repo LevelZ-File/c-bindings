19b42b8b40cd187803ef278e43cf19205f399a76
Gregory Mitchell <me@gmitch215.xyz>
2024-10-18 18:43:13 +0000

    Add Debugging for Manual Triggering

--- test/src/test.h
+++ test/src/test.h
@@ -1,7 +1,11 @@
 #ifndef LEVELZ_TEST_H
 #define LEVELZ_TEST_H
 
+int count = 1;
+
 int assert(int condition) {
+    printf("#%d: %s\n", count, condition ? "PASS" : "FAIL");
+    count++;
     return !condition;
 }
 
